4328cac3b1fc7a6f08134427dd5e1eb33e6d9c50
Kyle Rarey <kyle.rarey@gmail.com>
2024-01-14 15:07:23 -0500

    Add podman and avahi to system config

--- systems/framework.nix
+++ systems/framework.nix
@@ -16,15 +16,21 @@
     wget
   ];
 
+  # Manage network-related services
   programs.ssh.startAgent = true;
+  services.avahi.enable = true;
 
+  # Manage hardware-related services
   services.fwupd.enable = true;
   services.fprintd.enable = true;
-  services.flatpak.enable = true;
-
   # Using fstrim.timer for now, until Linux 6.2.x enforces discard=async on btrfs by default
   # Mostly because I don't want to change mount options and the iops threshold sysctl
   services.fstrim.enable = true;
+  
+  # Manage application-related services
+  services.flatpak.enable = true;
+  virtualisation.podman.enable = true;
+
 
   # No touchy
   system.stateVersion = "23.11"; # Did you read the comment?
