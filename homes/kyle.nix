65bfd27a2eb7d6dcc5eadcb65ba58134a9eca075
Kyle Rarey <kyle.rarey@gmail.com>
2024-01-14 13:22:29 -0500

    Manage shell configuration

--- homes/kyle.nix
+++ homes/kyle.nix
@@ -1,10 +1,50 @@
 { pkgs, unstable, ... }:
-
+let
+  sessionVariables = {
+    NOMAD_ADDR = "https://nomad.service.consul:4646";
+    VAULT_ADDR = "https://vault.service.consul:8200";
+    CONSUL_HTTP_ADDR = "https://consul.service.consul:8501";
+  };
+in
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "kyle";
   home.homeDirectory = "/home/kyle";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Kyle Rarey";
+    userEmail = "kyle.rarey@gmail.com";
+  };
+
+  programs.bash = {
+    inherit sessionVariables;
+    enable = true;
+  };
+
+  programs.zsh = {
+    inherit sessionVariables;
+    enable = true;
+  };
+
+  programs.starship.enable = true;
+
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  home.packages = [
+    unstable.nomad_1_7
+    unstable.vault
+    unstable.consul
+    unstable.terraform
+    pkgs.firefox
+    pkgs._1password
+    pkgs.vscode.fhs
+  ];
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -15,23 +55,4 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "23.11";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  programs.git = {
-    enable = true;
-    userName = "Kyle Rarey";
-    userEmail = "kyle.rarey@gmail.com";
-  };
-  
-  home.packages = [
-    unstable.nomad_1_7
-    unstable.vault
-    unstable.consul
-    unstable.terraform
-    pkgs.firefox
-    pkgs._1password
-    pkgs.vscode.fhs
-  ];
 }
--- systems/common.nix
+++ systems/common.nix
@@ -1,14 +1,17 @@
-{
+{ pkgs, ...}: {
   environment.localBinInPath = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   time.timeZone = "America/New_York";
 
   security.pki.certificateFiles = [ ../files/home-root.pem ];
 
+  programs.zsh.enable = true;
+
   users.users.kyle = {
     isNormalUser = true;
     description = "Kyle Rarey";
     extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIARgxhvXw0Pl1hPrimDE8YNcPpDhzelIMcTnMKBfHfZ/ kyle@opt.sh"
     ];
